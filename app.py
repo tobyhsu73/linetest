9e87b3b16eb4b26cc1e26ab6dbb8658825ea200a
tobyhsu73 <73582590+tobyhsu73@users.noreply.github.com>
2022-10-01 21:01:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from datetime import datetime
 
 from flask import Flask, abort, request
@@ -38,3 +39,12 @@ def handle_message(event):
     # Send To Line
     reply = TextSendMessage(text=f"{get_message}")
     line_bot_api.reply_message(event.reply_token, reply)
+        url = 'https://notify-api.line.me/api/notify'
+    token = 'xqH30BaWlOVwIj8JYd2uT6deJlp8FMJgHMkBMyOgA9j'
+    headers = {
+        'Authorization': 'Bearer ' + token    # 設定權杖
+    }
+    data = {
+        'message':'測試一下！'     # 設定要發送的訊息
+    }
+    data = requests.post(url, headers=headers, data=data)   # 使用 POST 方法
