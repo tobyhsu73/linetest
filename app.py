237dfb5771459e6b4d58525f3285a256ec4e954e
tobyhsu73 <73582590+tobyhsu73@users.noreply.github.com>
2022-10-02 14:09:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def handle_message(event):
         'Authorization': 'Bearer ' + token    # 設定權杖
     }
     data = {
-        'message': text     # 設定要發送的訊息
+        'message': f"{get_message}"     # 設定要發送的訊息
     }
     data = requests.post(url, headers=headers, data=data)   # 使用 POST 方法
     
