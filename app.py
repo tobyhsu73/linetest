68f0f5283c7793ddf9984b73bbec7288e9ce8085
tobyhsu73 <73582590+tobyhsu73@users.noreply.github.com>
2022-10-02 14:00:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,8 +35,10 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get_message = event.message.text
+    '''
     if re.match("你是誰",get_message):
         line_bot_api.reply_message(event.reply_token,TextSendMessage("才不告訴你勒~~")) 
+        '''
     # Send To Line
     reply = TextSendMessage(text=f"{get_message}")
     line_bot_api.reply_message(event.reply_token, reply)
